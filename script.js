65c3f8e5008db2155706d00084359006b55a66ec
MG <magallardo11@outlook.com>
2022-09-30 19:01:12 -0700

    setting up local storage

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
+var date = moment().format("LL");
 let fivedate = [];
 let fivetemp = [];
 let fivehumidity = [];
 let fivewind = [];
+// let storeCity = document.getElementByID("#storeCity")
+// let storeDate = document.getElementByID("#storeDate");
+// let storeTemp = document.getElementByID("#storeTemp");
+// let storeWind = document.getElementByID("#storeWind");
+// let storeHumidity = document.getElementByID("#storeHumidity")
+// let storeUVIndex = document.getElementByID("#storeUVIndex")
+// let storeIcon = document.getElementByID("#storeIcon")
+// let storeFiveTemp = document.getElementByID("#storeFiveTemp")
+// let storeFiveDate = document.getElementByID("#storeFiveDate")
+// let storeFiveWind = document.getElementByID("#storeFiveWind")
+// let storeFiveHumidity = document.getElementByID("#storeFiveHumidity")
+// let storeFiveIcon = document.getElementByID("#storeFiveICon")
+let sumbitCity = [];
+let localSearch = [];
+let btncontainer = document.querySelector(".button-container");
+
+function renderSearch() {
+    btncontainer.innerHTML = "";
+    // if (localStorage.length===0) {
+    //     return;
+    // }
+    for (i=localSearch.length - 1; i >= 0; i--) {
+        let btn = document.createElement("button");
+        btn.setAttribute("type", "button");
+        btn.classList.add("history-btn");
+        btn.setAttribute("data-search", localSearch[i]);
+        // btn.textContent = localSearch[i];
+        btn.innerHTML = localSearch[i];
+        btncontainer.append(btn)
+       
+        console.log(localSearch)
+        // $sumbitCity = JSON.parse((localStorage.getItem(localStorage.key(i)))).storeCity; $('#citylist').append(`<a href="#" class="list-group-item list-group-item-action list-group-item-info searchHistory">${sumbitCity}</a>`)
+    }
+}
+
+function addHistory(city) {
+    if (localSearch.length === -1) {
+        return;
+    } 
+    localSearch.push(city);
+    localStorage.setItem("search-history", JSON.stringify(localSearch));
+    renderSearch();
+}
+
+function initSearch() {
+    var saveSearch = localStorage.getItem("search-history");
+    if (saveSearch) {
+        localSearch = JSON.parse(saveSearch);
+    }
+    renderSearch();
+}
 
 $("#search-city").on("click", function (event) {
     event.preventDefault();
@@ -9,10 +61,11 @@ $("#search-city").on("click", function (event) {
     city = $("#input-group").val().trim().toUpperCase();
     console.log(city)
     let wAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=3c63a0dd5e51e96f081ed670e9c74ea9`
-    rendercitylatlon(wAPI);
+    rendercitylatlon(city);
 });
-var rendercitylatlon = function (url) {
-    fetch(url)
+var rendercitylatlon = function (city) {
+    let wAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=3c63a0dd5e51e96f081ed670e9c74ea9`
+    fetch(wAPI)
         .then(function (response) {
             return response.json();
         })
@@ -22,6 +75,7 @@ var rendercitylatlon = function (url) {
             long = data[0].lon
             rendercityUV(lat, long)
         });
+        addHistory(city)
 }
 
 var rendercityUV = function (lattitude, longitude) {
@@ -45,21 +99,43 @@ var rendercityUV = function (lattitude, longitude) {
                 fivehumidity[i] = data.daily[i + 1].humidity;
                 fivewind[i] = data.daily[i + 1].wind_speed;
             }
+// TODO: if local storage has data, get it and store it in a variable else make the variable equal an empty array For every search, we store the user's search into the array Then after we store it, we set that array into the local storage This way for every city the user searches, it keeps storing a city into the array
+
+            // if (localStorage(data).getItem() = localInput = []
+            // ) 
+            // localStorage.setItem('key', JSON.stringify({
+                    
+            // }))
+            // //     storeCity: sumbitCity,
+            // //     storeDate: date,
+            //     storeTemp: $(`#temperature`).text(),
+            //     storeWind: $(`#wind`),
+            //     storeHumidity: $(`#humidity`), 
+            //     storeUVIndex: $(`#uv`),
+            //     storeIconURL: 'test',
+            //     storeFiveTemp: fivetemp,
+            //     storeFiveDate: fivedate,
+            //     storeFiveWind: fivewind,
+            //     storeFiveHumidity: fivehumidity,
+            //     storeFiveIcon: 'test'
+            // 
+
             for (i = 0; i < 5; i++) {
                 $(`#fiveCard`).append(`
                 <div class="card col-3" style="width: 14rem;" id="info">
                 <div class="card-body">
-                    <h5 class="card-title">${fivedate[i]}</h5>
+                <h5 class="card-title">${fivedate[i]}</h5>
                 </div>
-                    <p class="list-group-item" >Temperature:${fivetemp[i]}</p>
-                    <p class="list-group-item" >Humidity:${fivehumidity[i]}</p>
-                    <p class="list-group-item" >Wind Speed:${fivewind[i]}</p>
+                <p class="list-group-item" >Temperature:${fivetemp[i]}</p>
+                <p class="list-group-item" >Humidity:${fivehumidity[i]}</p>
+                <p class="list-group-item" >Wind Speed:${fivewind[i]}</p>
                 </div>`); 
             }
             
         });
-}
-var date = moment().format("LL");
+       
+    
 
+};
 
 
